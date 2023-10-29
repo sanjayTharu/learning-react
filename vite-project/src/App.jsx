3d0939f05daa7095107d99576fd39765470364e4
Your Name <you@example.com>
2023-10-29 12:43:31 +0545

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const username='Sanjay'
+
+  return (
+    <>
+    <h1>Chai aur code with vite and sanjay</h1>
+    {/* <h1> this is evaluadted expression {username}</h1> */}
+    {/* <h1> this is evaluadted expression {username}</h1> */}
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+// function MyApp(){
+//   return(
+//     <div>
+//       <h2>Custom App</h2>
+//     </div>
+//   )
+// }
+
+// const reactElement=React.createElement(
+//   'a',
+//   {href:'https://google.com',target:'_blank'},
+//   'click me to visit google thank you'
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <App />
+)
